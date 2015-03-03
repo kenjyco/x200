67b0d8ca0d7e6a531b604f98ef8bded218c44772
Ken <kenjyco@gmail.com>
2015-03-02 22:01:19 -0600

    Don't install tmux 1.9a
    If you installed it, revert back to tmux 1.8 with the following
    commands at your shell prompt (the % sign):
    
        % sudo apt-get purge -y tmux
        % sudo apt-get update
        % sudo apt-get install -y tmux=1.8-5

--- sysinstall/mini-14.04/setup.bash
+++ sysinstall/mini-14.04/setup.bash
@@ -51,8 +51,8 @@ sudo apt-get update
 sudo apt-get install -y postgresql-9.4 postgresql-server-dev-9.4 postgresql-plpython-9.4 postgresql-contrib-9.4
 
 # http://stackoverflow.com/questions/25940944
-echo -e "\nTmux 1.9"
-sudo apt-get install -y python-software-properties software-properties-common
-sudo add-apt-repository -y ppa:pi-rho/dev
-sudo apt-get update
-sudo apt-get install -y tmux=1.9a-1~ppa1~t
+# echo -e "\nTmux 1.9"
+# sudo apt-get install -y python-software-properties software-properties-common
+# sudo add-apt-repository -y ppa:pi-rho/dev
+# sudo apt-get update
+# sudo apt-get install -y tmux=1.9a-1~ppa1~t
