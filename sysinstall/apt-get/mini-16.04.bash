334476ca6809717d3cdea1a9b7a5cf3520b4f7e4
Ken <kenjyco@gmail.com>
2016-11-06 23:05:28 -0600

    Update 'mint-18.0.bash' for MongoDB v3.2
    Explicitly specifying the 'xenial' codename (for for Ubuntu 16.04 LTS)
    since the package is not available for Linux Mint.

--- sysinstall/apt-get/mini-16.04.bash
+++ sysinstall/apt-get/mini-16.04.bash
@@ -63,7 +63,7 @@ sudo pip3 install uwsgi
 echo -e "\nRedis & MongodB stuff"
 sudo apt-get install -y redis-server
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
