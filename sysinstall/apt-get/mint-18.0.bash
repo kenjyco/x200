81c2f4b46432900eb4191d884009e985d15667d0
Ken <kenjyco@gmail.com>
2017-08-15 07:17:34 -0500

    Specifically use xenial in docker apt repo for mint

--- sysinstall/apt-get/mint-18.0.bash
+++ sysinstall/apt-get/mint-18.0.bash
@@ -67,7 +67,7 @@ sudo apt-get install -y default-jre
 echo -e "\nDocker stuff"
 sudo apt-get install -y software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker ${USER}
