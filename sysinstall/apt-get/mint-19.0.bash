2e2d29e7dbc34ba84ba6c8cacd32217c785c0daa
Ken <kenjyco@gmail.com>
2018-11-07 23:25:20 -0600

    Update docker and docker-compose installation for mint-19.0.bash

--- sysinstall/apt-get/mint-19.0.bash
+++ sysinstall/apt-get/mint-19.0.bash
@@ -65,13 +65,13 @@ sudo apt-get install -y mongodb-org
 sudo apt-get install -y default-jre
 
 echo -e "\nDocker stuff"
-sudo apt-get install -y software-properties-common
+sudo apt-get install -y software-properties-common apt-transport-https ca-certificates
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker ${USER}
-sudo su -c 'curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose'
+sudo su -c 'curl -L https://github.com/docker/compose/releases/download/1.23.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose'
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo -e "\nYarn stuff"
